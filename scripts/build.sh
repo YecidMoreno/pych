66f2a0973ff29bd86d6658c15aa3ad63b4c0c93e
YecidMoreno <YecidMoreno@usp.br>
2025-06-17 13:14:41 -0300

    build using xdocker

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,35 +4,51 @@ PARENT_DIR="$(dirname "$SCRIPT_DIR")"
 
 cd $PARENT_DIR
 
-ARCH=${1:-$(uname -m)}
+TARGET="${1:-}"
 NJOURNAL=${2:-$(nproc)}
 
-if ldd --version 2>&1 | grep -qi musl; then
-    PLATFORM="_musl"
-elif [ "$ARCH" = "x86_64" ]; then
-    PLATFORM="_amd64"
-else
-    PLATFORM=""
+if [ -z "${CXX}" ]; then
+    CXX="g++"
 fi
 
-TARGET="${ARCH}${PLATFORM}"
+THIS_MACHINE=$(${CXX} -dumpmachine)
+
+TARGET="${1:-$THIS_MACHINE}"
 TARGET="${TARGET,,}"
 
-CMD="cmake -B build/$TARGET -S . -DTARGET_PWD=$TARGET && cmake --build build/$TARGET -j$NJOURNAL"
-echo "Building for: $TARGET"
-echo $CMD
-
-
-if [ -z "$1" ]; then
+if [ "$THIS_MACHINE" = "$TARGET" ]; then
+    echo "----"
+    echo "THIS_MACHINE : ${THIS_MACHINE}"
+    echo "Building for : $TARGET"
+    CMD="cmake -B build/$TARGET -S . -DTARGET_MACHINE=$TARGET && "
+    CMD+="cmake --build build/$TARGET -j$NJOURNAL"
+    echo "----"
+    echo $CMD
+    echo "----"
     bash -c "$CMD"
     exit 0
 fi
 
-if [ "$ARCH" == "aarch64" ]; then
-    ./docker/aarch64.xdockcross bash -c "$CMD"
-elif [ "$TARGET" == "x86_64_musl" ]; then
-    ./docker/x86_64_musl.xdockcross bash -c "$CMD"
+CMD="source activate.sh && ./scripts/build.sh"
+BUILD_IMAGE=""
+
+case "$TARGET" in
+aarch64 | aarch64-unknown-linux-gnu)
+    BUILD_IMAGE="dockcross/linux-arm64:20250109-7bf589c"
+    ;;
+musl | x86_64-alpine-linux-musl)
+    BUILD_IMAGE="alpine-pych-x86_64:v0.1"
+    ;;
+*)
+    echo "Build not defined for: $TARGET" >&2
+    exit 1
+    ;;
+esac
+
+
+if [ -n "${BUILD_IMAGE}" ]; then
+    ./docker/build.xdockcross -i ${BUILD_IMAGE} bash -c "$CMD"
+    exit 0
 else
-    echo "not defined build for: $TARGET"
-    exit -1
+    exit 1
 fi
