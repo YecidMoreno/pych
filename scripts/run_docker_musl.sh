26867f0fa809b7315074ceb16b87314dc171ba89
YecidMoreno <YecidMoreno@usp.br>
2025-06-17 13:18:33 -0300

    add script to run PYCH-core in local docker

---
+++ scripts/run_docker_musl.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+BIN_DIR="release/x86_64-alpine-linux-musl"
+CONFIG_FILE="../../test/robot.json"
+
+CMD_SCRIPT="source activate.sh && cd $BIN_DIR && exec bash -c '$HH_CORE_EXEC -f $CONFIG_FILE'"
+
+docker/run.xdockcross -i alpine-pych-x86_64:v0.1 sh -c "$CMD_SCRIPT"
