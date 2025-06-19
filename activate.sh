49bad26dce1f3249cc74bb46ae18a8cefe768496
YecidMoreno <YecidMoreno@usp.br>
2025-06-19 13:07:58 -0300

    export SSH_ARGS

--- activate.sh
+++ activate.sh
@@ -3,17 +3,20 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 export TERM=xterm-256color
 
+export HH_CORE_EXEC="test/testCore"
+export HH_CORE_EXEC_ARG="-f robot.json"
+
 export HH_CORE_ACTIVATED="1"
 export HH_CORE_WORK="$(pwd)"
 
 export REMOTE_SSH_KEY="${HH_CORE_WORK}/ssh/id_rsa"
-
 export REMOTE_SSH_ARGS="-i ${REMOTE_SSH_KEY} "
 
-export REMOTE_ADDR="192.168.0.101"
+# export REMOTE_ADDR="192.168.0.101"
+export REMOTE_ADDR="raspberrypi.local"
 export REMOTE_USER="pi"
 export REMOTE_SSH="${REMOTE_USER}@${REMOTE_ADDR}"
-export REMOTE_WORK="/home/pi"
+export REMOTE_WORK="/home/pi/core"
 export REMOTE_LOCAL="${HH_CORE_WORK}/remote"
-export REMOTE_ARCH="aarch64"
+export REMOTE_ARCH="aarch64-unknown-linux-gnu"
 export PS1="(core)\[\e[1;32m\]\u@\h:\[\e[1;34m\]\w\[\e[0m\]\$ "
