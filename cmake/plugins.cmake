5d5cf6db92336112fdf11c6ec10c21a3857780a3
YecidMoreno <YecidMoreno@usp.br>
2025-08-19 16:20:07 -0300

    fix install

--- cmake/plugins.cmake
+++ cmake/plugins.cmake
@@ -9,5 +9,8 @@ function(pych_register_plugin target)
         ${PYCH_BUILD_INCLUDES}
         ${PYCH_INSTALL_INCLUDES}
     )
-    set(PYCH_PLUGINS_TARGET "${PYCH_PLUGINS_TARGET};${target}" CACHE INTERNAL "")
+    list(APPEND PYCH_PLUGINS_TARGET ${target})
+    list(REMOVE_DUPLICATES PYCH_PLUGINS_TARGET)
+    set(PYCH_PLUGINS_TARGET "${PYCH_PLUGINS_TARGET}" CACHE INTERNAL "")
+
 endfunction()
