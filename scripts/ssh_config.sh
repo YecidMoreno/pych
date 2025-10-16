c84bd137c3fcd2c21082367038e0446d97a3e083
YecidMoreno <YecidMoreno@usp.br>
2025-10-16 17:19:33 -0300

    fix ssh installer

--- scripts/ssh_config.sh
+++ scripts/ssh_config.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-if [ -n "$HH_CORE_ACTIVATED" ] &&
+if [ -n "$PYCH_CORE_ACTIVATED" ] &&
     [ -n "$REMOTE_ADDR" ] &&
     [ -n "$REMOTE_USER" ] &&
     [ -n "$REMOTE_WORK" ] &&
     [ -n "$REMOTE_ARCH" ]; then
     echo "Sending to: ${REMOTE_USER}@${REMOTE_ADDR}:${REMOTE_WORK}"
 else
-    exit -1
+    echo "exit 1: Please, activate pych first."
+    exit 1
 fi
 
-mkdir -p  "${HH_CORE_WORK}/ssh"
-cd ${HH_CORE_WORK}/ssh
+mkdir -p  "${PYCH_CORE_WORK}/ssh"
+cd ${PYCH_CORE_WORK}/ssh
 
-ssh-keygen -t rsa -b 4096 -C "hh_core" -f id_rsa
+ssh-keygen -t rsa -b 4096 -C "pych_core" -f id_rsa
+chmod 700 id_rsa
+chmod 700 id_rsa.pub
 
 ssh ${REMOTE_SSH} "mkdir -p ~/.ssh && chmod 700 ~/.ssh"
 # cat ${REMOTE_SSH_KEY}.pub | ssh ${REMOTE_SSH} "cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
 
 ssh-copy-id ${REMOTE_SSH_ARGS} ${REMOTE_SSH}
 
+exit $?
